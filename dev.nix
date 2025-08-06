6d32e0303aa94758835cc4e9f07721c2033716ac
ceeskamu <ceeskamu@gmail.com>
2025-08-06 02:08:43 +0000

    1

---
+++ dev.nix
@@ -0,0 +1,40 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [ 
+    pkgs.python3
+    pkgs.python312Packages.pip 
+    pkgs.chromedriver 
+    pkgs.chromium 
+    pkgs.curl
+    pkgs.liberation_ttf
+    pkgs.nss
+    pkgs.atk
+    pkgs.gtk3
+    pkgs.libxkbcommon
+    pkgs.xrandr-invert-colors
+    pkgs.alsa-lib
+    pkgs.cups
+    pkgs.libdrm
+    pkgs.libappindicator-gtk3
+    ];
+  services.docker.enable = true;
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [ "ms-python.python" "rangav.vscode-thunder-client" ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        install =
+          "python -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt";
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [ "README.md" "src/index.html" "main.py" ];
+      };
+      # Runs when a workspace is (re)started
+      onStart = { run-server = "./devserver.sh"; };
+    };
+  };
+}
